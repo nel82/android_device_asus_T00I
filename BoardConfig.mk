d4c1530f2e3d6a646be24c022e93ca7c66bc92a8
artefvck <artefvck07@gmail.com>
2016-03-18 18:38:32 +0700

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,28 @@
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# inherit from common cvtlp-common
+-include device/asus/cvtlp-common/BoardConfigCommon.mk
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/asus/T00I
+
+# Partitions
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1782579200 # 1.7GByte
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Use the non-open-source parts, if they're present
+-include vendor/asus/T00I/BoardConfigVendor.mk
---
+++ cm.mk
@@ -0,0 +1,31 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 800
+
+# Inherit some common CM stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/T00I/device.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/asus/T00I/overlay
+
+PRODUCT_RUNTIMES := runtime_libart_default
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_T00I
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_T00I
+PRODUCT_MANUFACTURER := asus
+PRODUCT_DEVICE := T00I
+
+TARGET_VENDOR_PRODUCT_NAME := WW_ZenFone
+TARGET_VENDOR_DEVICE_NAME := ASUS_T00I
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=ASUS_T00I PRODUCT_NAME=WW_ZenFone
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=WW_ZenFone \
+    BUILD_FINGERPRINT=asus/WW_ZenFone/ASUS_T00I:5.0/LRX21V/WW_ZenFone-V7.4.4-20150831:user/release-keys \
+    PRIVATE_BUILD_DESC="WW_ZenFone-user 5.0 LRX21V WW_ZenFone-V7.4.4-20150831 release-keys"
